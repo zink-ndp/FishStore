de5b6bbc74c8278d4268c67f6d2fb5d829c8fe47
vinhb2012055 <127917238+vinhb2012055@users.noreply.github.com>
2023-04-12 14:43:45 +0700

    update signin

--- login.php
+++ login.php
@@ -1,29 +1,55 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "shop_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 session_start();
-$tk = "" ;
-$mk = "" ;
-$kq = "";
-if(isset($_POST['submit']))
-{
-    require 'inc/myconnect.php';
-    $tk = $_POST['txtus'] ;
-    $mk = $_POST['txtem'];
-    $sql="SELECT * FROM loginuser  where email = '$tk'  and matkhau = '$mk'";
+    if( isset($_POST['submit']) ){
+    $tk = addslashes($_POST['txtus']) ;
+    $mk = addslashes($_POST['txtem']) ;
+
+    // $sql="select tk_id, tk_tendangnhap, tk_matkhau, tk_avatar, tk_vaitro from TAI_KHOAN  where tk_tendangnhap = '".strtolower($_POST['txtus'])."'  and tk_matkhau = '".$_POST['txtem']."'";
+    $sql="select tk_id, tk_tendangnhap, tk_matkhau, tk_avatar, tk_vaitro from TAI_KHOAN  where tk_tendangnhap = '".strtolower($tk)."'  and tk_matkhau = '".$mk."'";
+
     $result = $conn->query($sql);
-    // echo  $mk;
-    if($result->num_rows > 0){
-        while($row = $result->fetch_assoc()) {
-        $_SESSION['txtus'] = $tk ;
-        $_SESSION['HoTen'] = $row["HoTen"];
-        $_SESSION['email'] = $row["email"];
-        $_SESSION['dienthoai'] = $row["DienThoai"];
-            header('Location: index.php');
-            $row = $result->fetch_assoc();  
-        }         
+    if ($result->num_rows > 0) {
+        require 'inc/myconnect.php';
+        $row = $result->fetch_assoc();
+        $_SESSION["id"] = $row['tk_id'];
+        $_SESSION["pass"] = $row['tk_matkhau'];
+        $_SESSION["role"] = $row['tk_vaitro'];
+
+        $sql1 ="SELECT KH_ID, TK_ID, KH_HOTEN, KH_SDT, KH_EMAIL,KH_NGAYSINH,KH_DIACHI,KH_GIOITINH,KH_NGAYDK FROM KHACH_HANG where TK_ID = ".$_SESSION["id"]."";
+        $result1 = $conn->query($sql1);
+        if ($result1->num_rows > 0) {
+            $row1 = $result1->fetch_assoc();
+            $_SESSION["name"] = $row1['KH_HOTEN'];
+            $_SESSION["email"] = $row1['kh_email'];
+            $_SESSION["sdt"] = $row1['kh_sdt'];
+            $_SESSION["date"] =$row1['kh_ngaysinh'];
+            $_SESSION["location"] =$row1['kh_diachi'];
+            $_SESSION["sex"] =$row1['kh_gioitinh'];
+
+          }
+        if($_SESSION["role"]=='custommer'){
+            header('location: index.php');
+        }
+        else{
+            header('location: admin\dashboard.php');
+        }
     }
     else
     {
-        $kq ="Thông tin không đúng vui lòng kiềm tra lại";
+        $message = $_POST["txtus"] + $_POST["txtem"];
+        echo "<script type='text/javascript'>alert('$message');</script>";
+        header('Refresh: 10s;url=account.php');
+        }
     }
-}
 ?>
--- top.php
+++ top.php
@@ -30,13 +30,13 @@ $name ="Shop Cá Kiểng";
 					<ul class="top-link">
 						<?php
 							 // require "login.php";
-							      if(!isset($_SESSION['txtus'])) // If session is not set then redirect to Login Page
+							      if(!isset($_SESSION['id'])) // If session is not set then redirect to Login Page
 							       {
 							           printf(' <li><a href="account.php"><span class="glyphicon glyphicon-log-in"></span> Đăng nhập</a></li>
 										<li><a href="contact.php"><span class="glyphicon glyphicon-envelope"></span> Liên hệ</a></li>');
 							       }
 							       else{
-							       	echo '<li><span class="glyphicon glyphicon-user"></span> Xin chào ' ; echo '<span style="color:Tomato;"><b>' . $_SESSION['HoTen'] . '</b></span></li>' ;
+							       	echo '<li><span class="glyphicon glyphicon-user"></span> Xin chào ' ; echo '<span style="color:Tomato;"><b>' . $_SESSION['name'] . '</b></span></li>' ;
 							       	echo '<li><span class="glyphicon glyphicon-log-out"></span><a href="dangxuat.php"> Đăng xuất!</a></li>';
 							       }
 
